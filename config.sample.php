28e985a971c641a3d8ffb2a5c8f3478170c02fcf
esmartsave <malloy.nyakwama@gmail.com>
2025-11-19 17:13:36 +0200

    Update config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -1,4 +1,4 @@
 <?php
 // config.sample.php
 // Copy this file to config.php and put your real API key there
-define('NEWS_API_KEY', '59ed552825ab4d8694034b470796921d');
+//define('NEWS_API_KEY', '59ed552825ab4d8694034b470796921d');
